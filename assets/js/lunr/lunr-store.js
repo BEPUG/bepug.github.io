e876a1ce31bd674b420c9093be027290b439cb4c
Stijnc <stijn.callebaut@outlook.be>
2020-03-11 21:08:54 +0000

    20200311.2

---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,21 @@
+var store = [{
+        "title": "June 7, 2018 meetup",
+        "excerpt":"  Moving Your DSC Pull Server into the future by Ben Gelens  Flexible PowerShell logging without reinventing the wheel by Kim Oppalfens  My PowerShell development aids, Stijn CallebautHosted at itnetX Belgium Register here!Github slides and demo ","categories": ["meetup"],
+        "tags": [],
+        "url": "https://bepug.github.io/meetup/2018/06/07/meetup/",
+        "teaser":null},{
+        "title": "September 27, 2018 meetup",
+        "excerpt":"Infrastructure-as-Code: VSTS, Azure and the glue of PowerShell by Jeff Wouters Azure Cloud Shell and SHiPS by Steven Taylor Stick to the full potential of PowerShell by Christof Van Geendertaelen Function ctrl-c {} by Merlijn Van Waeyenberghe Too late for a complex ARM template by Ivo HaagenHosted at XylosRegister here!Github...","categories": ["meetup"],
+        "tags": [],
+        "url": "https://bepug.github.io/meetup/2018/09/27/meetup/",
+        "teaser":null},{
+        "title": "May 22, 2019 meetup",
+        "excerpt":"  PowerShell Core: Benefits and challenges by Aleksandar Nikolić  Build GUIs to make your code look super expensive by Merlijn Van WaeyenbergheHosted at Cheops (Antwerp offices) Register here! ","categories": ["meetup"],
+        "tags": [],
+        "url": "https://bepug.github.io/meetup/2019/05/22/meetup/",
+        "teaser":null},{
+        "title": "March 11, 2020 meetup",
+        "excerpt":"Modernizing PowerShell Scripts by Matthias van den Elsacker Git(Hub) 101 by Stijn &amp; MerlijnIntroducing peer to peer learning sessions open to all new PowerShell enthousiasts. Bring your laptop and show us the PowerShell challenge you’re struggling with. We’ll do our best to help you understand the concepts and guide you...","categories": ["meetup"],
+        "tags": [],
+        "url": "https://bepug.github.io/meetup/2020/03/11/meetup/",
+        "teaser":null}]
